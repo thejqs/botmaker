b04434b93bb3ed5b5972bafd7d0599fa761722c4
Jacob Sanders <j.q.sanders@gmail.com>
2015-09-21 15:13:59 -0600

    handling Twitter auth, updating .gitignore

---
+++ project/settings_local.py
@@ -0,0 +1,30 @@
+# go get a secret key from, say, http://www.miniwebtool.com/django-secret-key-generator/
+SECRET_KEY = 'PUT IT IN THIS STRING'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'NAME OF YOUR DATABASE',
+        'HOST': '127.0.0.1',
+        'USER': 'YOUR USER',
+        'PASSWORD': 'YOUR DB PASSWORD',
+        'PORT': '',
+    }
+}
+
+# email settings for gmail, which you might not need. But if you do:
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'YOUR EMAIL@gmail.com'
+EMAIL_HOST_PASSWORD = 'YOUR PASSWORD'
+EMAIL_USE_TLS = True
+
+# When you register your app with Twitter, you'll need these. 
+# Best to make sure they're not public.
+# Get them from apps.twitter.com when you create your bot account.
+auth_settings = {
+    'consumer_key': 'BUNCH OF GIBBERISH',
+    'consumer_secret': 'BUNCH OF GIBBERISH',
+    'access_token_key': 'BUNCH OF GIBBERISH',
+    'access_token_secret': 'BUNCH OF GIBBERISH'
+}
